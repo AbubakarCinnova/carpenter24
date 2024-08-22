0431209e3f7df1ea32e63b306a244f3850182426
AbubakarCinnova <amahboob@cinnova.com>
2024-08-22 15:28:59 +0500

    Update mini-splits.php

--- mini-splits.php
+++ mini-splits.php
@@ -118,16 +118,13 @@
                                         solution for your unique needs.</p>
 
                                     <!-- Embedded YouTube Video -->
-                                    <div
-                                        style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto;">
-                                        <iframe src="https://www.youtube.com/embed/1amhrr52hvM?t=51s"
+                                    <div>
+                                        <iframe width="560" height="315" class="video-responsive"
+                                            src="https://www.youtube.com/embed/1amhrr52hvM?t=51s"
                                             title="Lennox Mini-Split Heat Pumps" frameborder="0"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                            allowfullscreen
-                                            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
-                                        </iframe>
+                                            allowfullscreen></iframe>
                                     </div>
-
 
                                     <h2>Experience the Carpenter Difference</h2>
                                     <p>Contact us today to discover how we’ve been building lasting customer
