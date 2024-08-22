3e6d7c1270e423f28177718818965f20f08bd55c
AbubakarCinnova <amahboob@cinnova.com>
2024-08-22 15:06:10 +0500

    Create maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,208 @@
+<?php include('header.php'); ?>
+<title>Carpenter Heating and Air Conditioning | Annual V.I.P Maintenance Program </title>
+
+<meta name="description"
+    content="Join the Carpenter Heating and Air Conditioning Annual V.I.P Maintenance Program in Ogden, Utah, and enjoy priority service, energy savings, and extended equipment life. Contact us today to learn more about our HVAC maintenance plan.">
+
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="float-text show-on-scroll">
+            <span><a href="#">Scroll to top</a></span>
+        </div>
+        <div class="scrollbar-v show-on-scroll"></div>
+
+        <!-- page preloader begin -->
+        <!--  <div id="de-loader"></div> -->
+        <!-- page preloader close -->
+
+        <?php include('menu.php'); ?>
+
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+            <!-- section begin -->
+            <section id="subheader" class="jarallax text-light">
+                <img src="images/background/" class="jarallax-img" alt="">
+                <div class="container relative z-index-1000">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="subtitle s2 bg-color text-light wow fadeInUp mb-2">Our Service</div>
+                            <h1>Maintenance</h1>
+                            <ul class="crumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">Maintenance</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="de-overlay"></div>
+            </section>
+            <!-- section close -->
+
+            <div class="bg-color relative z-index-1000 mt-40 mb40">
+                <a href="contact.php">
+                    <img src="images/svg/phone-calling-svgrepo-com.svg"
+                        class="border-white-6 fs-30 bg-color w-80px p-3 mb20 circle absolute abs-center sm-hide" alt="">
+                </a>
+            </div>
+
+            <section>
+                <div class="container">
+                    <div class="row g-5">
+
+
+                        <div class="col-lg-12">
+                            <div class="row g-4">
+                                <div class="col-lg-12">
+
+                                    <h2>Annual V.I.P Maintenance Program</h2>
+                                    <p>As your heating and cooling equipment ages, the value of a maintenance program
+                                        increases. The savings from just one major repair could exceed the cost of the
+                                        service agreement itself. Regular service for your equipment is like changing
+                                        the oil in your vehicle—it can save you thousands in energy costs and future
+                                        repairs, as well as extend the life of your HVAC system.</p>
+
+                                    <h2>Top Priority Status</h2>
+                                    <p>As a member of the Annual V.I.P Maintenance Program, you are a Carpenter Heating
+                                        and Air Conditioning VIP customer. This means when you need service, you get it
+                                        NOW—no lengthy waits when your system is out of order. Carpenter offers
+                                        emergency service 24 hours a day, every day of the week.</p>
+
+                                    <h2>Longer Equipment Life</h2>
+                                    <p>The special care you receive with the V.I.P Maintenance Program will help your
+                                        heating and cooling equipment last longer and run more efficiently.</p>
+
+                                    <h2>Energy Savings</h2>
+                                    <p>The money you'll save by maintaining your equipment in top running order with a
+                                        V.I.P Maintenance Program membership will add up to significant savings on your
+                                        heating and cooling costs.</p>
+
+                                    <h2>Fulfillment of Warranty Requirements</h2>
+                                    <p>If your HVAC components are under warranty, you should know that most
+                                        manufacturers require evidence of annual maintenance to honor a claim. Investing
+                                        in regular maintenance is essentially like having an insurance policy. If
+                                        something malfunctions that is covered under warranty, you’ll be able to file
+                                        your claim with confidence and ease, supported by evidence of annual
+                                        maintenance.</p>
+
+                                    <h2>What's Included?</h2>
+                                    <p>Your yearly maintenance agreement includes priority scheduling, discounts on
+                                        service calls, and protection for your HVAC equipment. You can individually
+                                        protect your furnace and air conditioner, or choose combined protection for both
+                                        at a reduced price.</p>
+
+                                    <h3>Annual V.I.P Maintenance Program Benefits</h3>
+                                    <ul>
+                                        <li>10% Off all Service Calls</li>
+                                        <li>Multiple System Discounts</li>
+                                        <li>Receive a 5% discount on installed equipment during the contract</li>
+                                        <li>No Overtime Charges</li>
+                                        <li>Agreement is Transferrable</li>
+                                        <li>Priority Service Scheduling</li>
+                                    </ul>
+
+                                    <h3>Furnace Tune-Up</h3>
+                                    <ul>
+                                        <li>Furnace Inspection</li>
+                                        <li>Test furnace for carbon monoxide leaks</li>
+                                        <li>Clean ignition assembly</li>
+                                        <li>Check flue draft</li>
+                                        <li>Oil inducer and blower motor</li>
+                                        <li>Check burner</li>
+                                        <li>Measure volt amps on all motors</li>
+                                        <li>Check & tighten electrical connections</li>
+                                        <li>Test safety controls</li>
+                                        <li>Check thermostat</li>
+                                        <li>Check humidifier</li>
+                                        <li>Check filters</li>
+                                        <li>Clean flame sensor</li>
+                                        <li>Start and check all equipment</li>
+                                        <li>Check pilot assemblies</li>
+                                        <li>Check condensate drain</li>
+                                        <li>Change humidifier pad</li>
+                                    </ul>
+
+                                    <h3>Air Conditioner Protection</h3>
+
+                                    <h4>Air Conditioner Tune-Up</h4>
+                                    <ul>
+                                        <li>Condensing Unit Inspection</li>
+                                        <li>Check operating pressures</li>
+                                        <li>Check for leaks</li>
+                                        <li>Clean condenser coil</li>
+                                        <li>Check the evaporator coil for proper drainage</li>
+                                        <li>Check drain line</li>
+                                        <li>Check filter</li>
+                                        <li>Check amp draw</li>
+                                        <li>Check blower</li>
+                                        <li>Check refrigerant</li>
+                                        <li>Check if the unit is level</li>
+                                        <li>Check temperature</li>
+                                        <li>Shut down the humidifier</li>
+                                        <li>Cycle the thermostat</li>
+                                        <li>Check the fuses</li>
+                                        <li>Secure all electrical connections</li>
+                                    </ul>
+
+                                    <h3>Terms of Agreement</h3>
+                                    <p>All Carpenter Heating and Air Conditioning V.I.P. Maintenance Program contracts
+                                        run for one calendar year from the invoice date.</p>
+
+                                    <h4>Multiple-year discounts are available:</h4>
+                                    <ul>
+                                        <li>2 Years - 5% Discount</li>
+                                        <li>3 Years - 10% Discount</li>
+                                    </ul>
+                                    <p>The per-year price is for a standard single-family home. Parts are an additional
+                                        charge.</p>
+
+                                    <hr>
+
+                                    <p><strong>Experience the Carpenter Difference—Contact Us Today to Discover How We
+                                            Build Lasting Customer Relationships on a Foundation of Service.</strong>
+                                    </p>
+
+
+
+                                </div>
+
+                                <!--  <div class="col-lg-6">
+                                    <img src="images/misc/services-2.webp" class="img-fluid rounded-20px mb-4" alt="">
+                                    <img src="images/misc/services-3.webp" class="img-fluid rounded-20px" alt="">
+                                </div> -->
+                            </div>
+
+                            <div class="spacer-double"></div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="bg-color text-light pt40 pb40">
+                <div class="container">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-lg-9">
+                            <h3 class="mb-1"><i class="icofont-check-circled fs-32 me-3 id-color-2"></i>Carpenter
+                                Heating and Cooling</h3>
+                        </div>
+
+                        <div class="col-lg-3 text-lg-end text-start">
+                            <a class="btn-main bg-color-2" href="contact.php">Schedule Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- content close -->
+
+
+        <?php include('footer.php'); ?>
