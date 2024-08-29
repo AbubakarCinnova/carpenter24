a280064d453b2c49e3235b744496053530d49be1
AbubakarCinnova <amahboob@cinnova.com>
2024-08-29 11:13:54 +0500

    4 pages created

---
+++ south-weber.php
@@ -0,0 +1,95 @@
+<?php include('header.php'); ?>
+
+<title>Trusted Heating and Cooling Services in South Weber, Utah | Carpenter Heating and Air Conditioning</title>
+
+
+<meta name="description"
+    content="Reliable HVAC services in South Weber, Utah. Carpenter Heating and Air Conditioning offers expert furnace and AC repair, installation, and maintenance. Over 65 years of trusted service. Contact us today!" />
+
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="float-text show-on-scroll">
+            <span><a href="#">Scroll to top</a></span>
+        </div>
+        <div class="scrollbar-v show-on-scroll"></div>
+
+        <!-- page preloader begin -->
+        <!--  <div id="de-loader"></div> -->
+        <!-- page preloader close -->
+
+        <?php include('menu.php'); ?>
+
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+            <!-- section begin -->
+            <section id="subheader" class="jarallax text-light">
+                <img src="images/background/" class="jarallax-img" alt="heating">
+                <div class="container relative z-index-1000">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="subtitle s2 bg-color text-light wow fadeInUp mb-2">Our Service</div>
+                            <h1>South Weber</h1>
+                            <ul class="crumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">South Weber</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="de-overlay"></div>
+            </section>
+            <!-- section close -->
+
+            <div class="bg-color relative z-index-1000 mt-40 mb40">
+                <a href="contact.php">
+                    <img src="images/svg/phone-calling-svgrepo-com.svg"
+                        class="border-white-6 fs-30 bg-color w-80px p-3 mb20 circle absolute abs-center sm-hide" alt="">
+                </a>
+            </div>
+
+            <section>
+                <div class="container">
+                    <div class="row g-5">
+                        <div class="col-lg-12">
+                            <div class="row g-4">
+                                <div class="col-lg-12">
+
+
+
+
+
+                                </div>
+
+
+                            </div>
+
+                            <div class="spacer-double"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="bg-color text-light pt40 pb40">
+                <div class="container">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-lg-9">
+                            <h3 class="mb-1"><i class="icofont-check-circled fs-32 me-3 id-color-2"></i>Carpenter
+                                Heating and Cooling</h3>
+                        </div>
+
+                        <div class="col-lg-3 text-lg-end text-start">
+                            <a class="btn-main bg-color-2" href="contact.php">Schedule Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- content close -->
+
+
+        <?php include('footer.php'); ?>
