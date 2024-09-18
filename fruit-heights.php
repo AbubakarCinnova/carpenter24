ecc186e5f18ae9673010b361ce8cc6aec7a44e31
AbubakarCinnova <amahboob@cinnova.com>
2024-09-18 10:49:09 +0500

    Service area images updated

--- fruit-heights.php
+++ fruit-heights.php
@@ -25,7 +25,7 @@
             <div id="top"></div>
             <!-- section begin -->
             <section id="subheader" class="jarallax text-light">
-                <img src="images/background/" class="jarallax-img" alt="heating">
+                <img src="images/background/FruitHeights, Utah (1).webp" class="jarallax-img" alt="heating">
                 <div class="container relative z-index-1000">
                     <div class="row align-items-center">
                         <div class="col-lg-6">
