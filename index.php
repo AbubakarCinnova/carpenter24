b66ba8734f5d41fc0af56e35ec6c758530e8f6e7
AbubakarCinnova <amahboob@cinnova.com>
2024-09-18 10:57:24 +0500

    Update index.php

--- index.php
+++ index.php
@@ -415,7 +415,7 @@
                                     <div class="col-12 text-center">
                                         <div class="bg-color text-light px-4 pt30 pb10 rounded-10 wow fadeInLeft">
                                             <div class="de_count">
-                                                <h3><span class="timer" data-to="133,620" data-speed="3000"></span>+
+                                                <h3><span class="timer" data-to="133620" data-speed="3000"></span>+
                                                 </h3>
                                                 <h4>Service Calls</h4>
                                             </div>
